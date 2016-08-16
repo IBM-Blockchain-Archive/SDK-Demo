8d2c14d35568a22644bfe0ed5715799d4cafaaef
ratnakar <rasara@us.ibm.com>
2016-08-16 17:04:41 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,59 @@
+#How to use HFC with Bluemix networks
+
+The goal is to run `helloblockchain.js` sample program, which will deploy example02 chaincode and query/invoke it.
+
+1. Install HFC v0.5.0:
+
+	```
+	> npm install hfc@0.5.0
+	```
+
+1. Get the Network credentials and save as ServiceCredentials.json (Refer the sample file in repo)
+
+1. Browse to `./node_modules/hfc` (this will be referred to as the "root" of the hfc module)
+
+1. Open `helloblockchain.js` and look for what you are setting as the "key value store" ie `chain.setKeyValStore(hfc.newFileKeyValStore('/tmp/keyValStore'));`
+	1. Find your key value store locally and make sure the path to the folder exists
+		- (in windows default is) C:\tmp\keyValStore
+	1. Also delete anything in this `keyValStore` folder (you should empty this out anytime you connect to a new network or switch users)
+
+1. Create `$GOPATH/src/github.com/chaincode_example02` folder
+	- copy `chaincode_example02.go` from [here](https://github.com/hyperledger/fabric/blob/master/examples/chaincode/go/chaincode_example02/chaincode_example02.go)
+
+1. Copy the `vendor.7z` file from this repo  to `$GOPATH/src/github.com/chaincode_example02` and **unzip**
+	- delete `vendor.7z`
+
+1. From root of hfc module run:
+	
+```
+	> set DEBUG=hfc
+	> node helloblockchain.js -c $GOPATH/go/src/github.com/chaincode_example02
+```
+Success looks like:
+
+```
+Successfully deployed chaincode: request={"fcn":"init","args":["a","100","b","200"],"certificatePath":"/certs/blockchain-cert.pem","chaincodePath":"github.com/chaincode_example02/"}, response={"uuid":"2d6ad8d6-1390-4c60-a01b-f4c301175eb7","chaincodeID":"9be0a0ed3f1788e8728c8911c747d2f6d0e205fa63422dc598d498fe709b9b8d","result":"TODO: get actual results; waited 120 seconds and assumed deploy was successful"}
+
+Successfully submitted chaincode invoke transaction: request={"chaincodeID":"9be0a0ed3f1788e8728c8911c747d2f6d0e205fa63422dc598d498fe709b9b8d","fcn":"invoke","args":["a","b","1"]}, response={"uuid":"f9a902d2-44d8-4b68-b43d-419470ba73ae"}
+
+Successfully completed chaincode invoke transaction: request={"chaincodeID":"9be0a0ed3f1788e8728c8911c747d2f6d0e205fa63422dc598d498fe709b9b8d","fcn":"invoke","args":["a","b","1"]}, response={"result":"waited 20 seconds and assumed invoke was successful"}
+
+Successfully queried  chaincode function: request={"chaincodeID":"9be0a0ed3f1788e8728c8911c747d2f6d0e205fa63422dc598d498fe709b9b8d","fcn":"query","args":["a"]}, value=99
+```
+
+***
+
+#Troubleshoot
+- if you get query failure error as below. 
+
+  ```
+Failed to query chaincode, function: request={"chaincodeID":"9be0a0ed3f1788e8728c8911c747d2f6d0e205fa63422dc598d498fe709b9b8d","fcn":"query","args":["a"]}, error={"error":{"status":"FAILURE","msg":{"type":"Buffer","data":[69,114,114,111,114,58,70,97,105,108,101,100,32,116,111,32,108,97,117,110,99,104,32,99,104,97,105,110,99,111,100,101,32,115,112,101,99,40,112,114,101,109,97,116,117,114,101,32,101,120,101,99,117,116,105,111,110,32,45,32,99,104,97,105,110,99,111,100,101,32,40,57,98,101,48,97,48,101,100,51,102,49,55,56,56,101,56,55,50,56,99,56,57,49,49,99,55,52,55,100,50,102,54,100,48,101,50,48,53,102,97,54,51,52,50,50,100,99,53,57,56,100,52,57,56,102,101,55,48,57,98,57,98,56,100,41,32,105,115,32,98,101,105,110,103,32,108,97,117,110,99,104,101,100,41]}},"msg":"Error:Failed to launch chaincode spec(premature execution - chaincode (9be0a0ed3f1788e8728c8911c747d2f6d0e205fa63422dc598d498fe709b9b8d) is being launched)"}
+  ```
+
+  increase deploy wait time
+
+`chain.setDeployWaitTime(80);`
+
+- if you get a handshake error, try a different `grpc` version
+- if you cannot npm install the hfc module, remove sleep dependency in package.json and delete node_modules/sleep if it exists
+
---
+++ helloblockachain.js
@@ -0,0 +1,232 @@
+var hfc = require('hfc');
+var util = require('util');
+var fs = require('fs');
+const https = require('https');
+
+// Create a client blockchin.
+var chain = hfc.newChain("targetChain");
+
+// Configure the KeyValStore which is used to store sensitive keys.
+// This data needs to be located or accessible any time the users enrollmentID
+// perform any functions on the blockchain.  The users are not usable without
+// This data.
+// Please ensure you have a /tmp directory prior to placing the keys there.
+// If running on windows or mac please review the path setting.
+chain.setKeyValStore(hfc.newFileKeyValStore('/tmp/keyValStore'));
+
+// Creating an environment variable for ciphersuites
+process.env['GRPC_SSL_CIPHER_SUITES'] = 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES256-GCM-SHA384';
+
+var ccPath;
+if (process.argv.length != 4) {
+    console.log("Invalid arguments");
+    console.log("USAGE: node helloblockachain.js -c <path-to-chaincode>");
+    console.log("ex: ");
+    console.log("node helloblockachain.js -c $GOPATH/src/github.com/chaincode_example02");
+    process.exit();
+}
+process.argv.forEach(function(val, index, array) {
+    if (index == 2 && (!val || val != "-c")) {
+        console.log("Invalid arguments")
+        process.exit();
+    } else if (index == 3) {
+        if (!val) {
+            console.log("Invalid arguments")
+            process.exit();
+        } else {
+            ccPath = val;
+        }
+    }
+});
+
+// Read and process the credentials.json
+var network;
+try {
+    network = JSON.parse(fs.readFileSync('ServiceCredentials.json', 'utf8'));
+} catch (err) {
+    console.log("ServiceCredentials.json is missing, Rerun once the file is available")
+    process.exit();
+}
+
+var peers = network.credentials.peers;
+var users = network.credentials.users;
+
+// Determining if we are running on a startup or HSBN network based on the url
+// of the discovery host name.  The HSBN will contain the string zone.
+var isHSBN = peers[0].discovery_host.indexOf('zone') >= 0 ? true : false;
+var peerAddress = [];
+var network_id = Object.keys(network.credentials.ca);
+var ca_url = "grpcs://" + network.credentials.ca[network_id].discovery_host + ":" + network.credentials.ca[network_id].discovery_port;
+
+if (!isHSBN) {
+    //HSBN uses RSA generated keys
+    chain.setECDSAModeForGRPC(true)
+}
+
+var certFile = 'certificate.pem';
+var certUrl = network.credentials.cert;
+fs.access(certFile, (err) => {
+    if (!err) {
+        console.log("\nDeleting existing certificate ", certFile);
+        fs.unlinkSync(certFile);
+    }
+    downloadCertificate();
+});
+
+function downloadCertificate() {
+    https.get(certUrl, (res) => {
+        console.log('\nDownloading certificate ', certFile);
+
+        res.on('data', (d) => {
+            fs.appendFile(certFile, d, (err) => {
+                if (err) throw err;
+            });
+        });
+        res.on('end', function() {
+            fs.appendFile(certFile, "\n", (err) => {
+                if (err) throw err;
+                //fs.createReadStream('certificate.pem').pipe(fs.createWriteStream(ccPath+'/certificate.pem'));
+                fs.writeFileSync(ccPath + '/certificate.pem', fs.readFileSync('certificate.pem'));
+                enrollAndRegisterUsers();
+            });
+        });
+    }).on('error', (e) => {
+        console.error(e);
+        process.exit();
+    });
+}
+
+function enrollAndRegisterUsers() {
+    var cert = fs.readFileSync(certFile);
+    chain.setMemberServicesUrl(ca_url, {
+        pem: cert
+    });
+
+    // Adding all the peers to blockchain
+    // this adds high availability for the client
+    for (var i = 0; i < peers.length; i++) {
+        chain.addPeer("grpcs://" + peers[i].discovery_host + ":" + peers[i].discovery_port, {
+            pem: cert
+        });
+    }
+
+    console.log("\n\n------------- peers and caserver information: -------------");
+    console.log(chain.getPeers());
+    console.log(chain.getMemberServices());
+    console.log('-----------------------------------------------------------\n\n');
+    var testChaincodeID;
+
+    // Enroll a 'admin' who is already registered because it is
+    // listed in fabric/membersrvc/membersrvc.yaml with it's one time password.
+    chain.enroll(users[0].username, users[0].secret, function(err, admin) {
+        if (err) throw Error("\nERROR: failed to enroll admin : %s", err);
+
+        console.log("\nEnrolled admin sucecssfully");
+
+        // Set this user as the chain's registrar which is authorized to register other users.
+        chain.setRegistrar(admin);
+
+        var enrollName = "JohnDoe"; //creating a new user
+        var registrationRequest = {
+            enrollmentID: enrollName,
+            account: "group1",
+            affiliation: "00001"
+        };
+        chain.registerAndEnroll(registrationRequest, function(err, user) {
+            if (err) throw Error(" Failed to register and enroll " + enrollName + ": " + err);
+
+            console.log("\nEnrolled and registered " + enrollName + " successfully");
+
+            //setting timers for fabric waits
+            chain.setDeployWaitTime(120);
+            chain.setInvokeWaitTime(20);
+            console.log("\nDeploying chaincode ...")
+            deployChaincode(user);
+        });
+    });
+}
+
+function deployChaincode(user) {
+    // Construct the deploy request
+    var deployRequest = {
+        // Function to trigger
+        fcn: "init",
+        // Arguments to the initializing function
+        args: ["a", "100", "b", "200"],
+        // the location where the startup and HSBN store the certificates
+        certificatePath: isHSBN ? "/root/" : "/certs/blockchain-cert.pem"
+    };
+    deployRequest.chaincodePath = "github.com/chaincode_example02/";
+
+    // Trigger the deploy transaction
+    var deployTx = user.deploy(deployRequest);
+
+    // Print the deploy results
+    deployTx.on('complete', function(results) {
+        // Deploy request completed successfully
+        testChaincodeID = results.chaincodeID;
+        console.log("\nChaincode ID : " + testChaincodeID);
+        console.log(util.format("\nSuccessfully deployed chaincode: request=%j, response=%j", deployRequest, results));
+        invokeOnUser(user);
+    });
+
+    deployTx.on('error', function(err) {
+        // Deploy request failed
+        console.log(util.format("\nFailed to deploy chaincode: request=%j, error=%j", deployRequest, err));
+    });
+}
+
+function invokeOnUser(user) {
+    // Construct the invoke request
+    var invokeRequest = {
+        // Name (hash) required for invoke
+        chaincodeID: testChaincodeID,
+        // Function to trigger
+        fcn: "invoke",
+        // Parameters for the invoke function
+        args: ["a", "b", "1"]
+    };
+
+    // Trigger the invoke transaction
+    var invokeTx = user.invoke(invokeRequest);
+
+    // Print the invoke results
+    invokeTx.on('submitted', function(results) {
+        // Invoke transaction submitted successfully
+        console.log(util.format("\nSuccessfully submitted chaincode invoke transaction: request=%j, response=%j", invokeRequest, results));
+    });
+    invokeTx.on('complete', function(results) {
+        // Invoke transaction completed successfully
+        console.log(util.format("\nSuccessfully completed chaincode invoke transaction: request=%j, response=%j", invokeRequest, results));
+        queryUser(user);
+    });
+    invokeTx.on('error', function(err) {
+        // Invoke transaction submission failed
+        console.log(util.format("\nFailed to submit chaincode invoke transaction: request=%j, error=%j", invokeRequest, err));
+    });
+}
+
+function queryUser(user) {
+    // Construct the query request
+    var queryRequest = {
+        // Name (hash) required for query
+        chaincodeID: testChaincodeID,
+        // Function to trigger
+        fcn: "query",
+        // Existing state variable to retrieve
+        args: ["a"]
+    };
+
+    // Trigger the query transaction
+    var queryTx = user.query(queryRequest);
+
+    // Print the query results
+    queryTx.on('complete', function(results) {
+        // Query completed successfully
+        console.log("\nSuccessfully queried  chaincode function: request=%j, value=%s", queryRequest, results.result.toString());
+    });
+    queryTx.on('error', function(err) {
+        // Query failed
+        console.log("\nFailed to query chaincode, function: request=%j, error=%j", queryRequest, err);
+    });
+}
